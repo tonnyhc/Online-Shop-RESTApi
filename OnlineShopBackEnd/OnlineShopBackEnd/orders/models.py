6b34fe30139f37f985bda32f76aa51677e213eae
Toni Petrov <smokercho56@gmail.com>
2023-02-27 20:16:28 +0200

    Created model for the orders

--- OnlineShopBackEnd/OnlineShopBackEnd/orders/models.py
+++ OnlineShopBackEnd/OnlineShopBackEnd/orders/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import get_user_model
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Order(models.Model):
+    MAX_LEN_FULL_NAME = 120
+    MAX_LEN_PHONE_NUMBER = 10
+    MIN_LEN_PHONE_NUMBER = 10
+    MAX_LEN_TOWN_NAME = 50
+    MIN_LEN_TOWN_NAME = 3
+    MAX_LEN_ADDRESS = 100
+    MIN_LEN_ADDRESS = 3
+    MAX_LEN_POST_CODE = 5
+    MIN_LEN_POST_CODE = 4
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE
+    )
+
+    full_name = models.CharField(
+        max_length=MAX_LEN_FULL_NAME
+    )
+
+    phone_number = models.CharField(
+        max_length=MAX_LEN_PHONE_NUMBER,
+        validators=[
+            validators.MinLengthValidator(MIN_LEN_PHONE_NUMBER),
+            validators.RegexValidator('08[789]\d{7}', 'The phone number you entered is invalid!')
+        ]
+    )
+
+    town = models.CharField(
+        max_length=MAX_LEN_TOWN_NAME,
+        validators=[validators.MinLengthValidator(MIN_LEN_TOWN_NAME)]
+    )
+
+    address = models.CharField(
+        max_length=MAX_LEN_ADDRESS,
+        validators=[validators.MinLengthValidator(MIN_LEN_ADDRESS)]
+    )
+
+    post_code = models.CharField(
+        max_length=MAX_LEN_POST_CODE,
+        validators=[
+            validators.MinLengthValidator(MIN_LEN_POST_CODE),
+            validators.integer_validator
+        ]
+    )
